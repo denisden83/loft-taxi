cb052512b335b39556e3656a875d60163df89a75
denys <denisden83@gmail.com>
2021-02-14 17:36:56 +0100

    feat(map): create Map component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import {Component} from "react";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
+import Map from "./Map";
 
 class App extends Component {
   render() {
     return (
-      <SignUp/>
+      <Map/>
     );
   }
 }
