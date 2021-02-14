0ee482c145361f230720ed7ce23af637c6fc6f34
denys <denisden83@gmail.com>
2021-02-14 18:04:56 +0100

    feat(profile): create Profile component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import {Component} from "react";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
 import Map from "./Map";
+import Profile from "./Profile";
 
 class App extends Component {
   render() {
     return (
-      <Map/>
+      <Profile />
     );
   }
 }
