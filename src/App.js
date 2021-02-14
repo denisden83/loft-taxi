20bfe7199a3b1677ffad1d4667e29237d56895fb
denys <denisden83@gmail.com>
2021-02-14 17:24:17 +0100

    feat(signup): create SignUp component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {Component} from "react";
 import LogIn from "./LogIn";
+import SignUp from "./SignUp";
 
 class App extends Component {
   render() {
     return (
-      <LogIn/>
+      <SignUp/>
     );
   }
 }
