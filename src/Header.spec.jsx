e93e8aa1d444d5c110236dc9382e12114e149b32
denys <denisden83@gmail.com>
2021-02-16 21:23:06 +0100

    test(header): create header test

---
+++ src/Header.spec.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from "@testing-library/react";
+import Header from "./Header";
+
+describe("Header", () => {
+  it('should render correctly', function () {
+    const {getByText} = render(<Header/>);
+    expect(getByText('Log out')).toBeInTheDocument();
+  });
+});
