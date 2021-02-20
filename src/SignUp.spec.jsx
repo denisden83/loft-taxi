85bcbd6ffb92f0a046f9cd415c7112630a288389
denys <denisden83@gmail.com>
2021-02-20 20:32:08 +0100

    test(sign up): correct sign up page tests

--- src/SignUp.spec.jsx
+++ src/SignUp.spec.jsx
@@ -5,9 +5,9 @@ import SignUp from "./SignUp";
 describe("SignUp", () => {
   it('should render correctly', function () {
     const {getByPlaceholderText} = render(<SignUp/>);
-    expect(getByPlaceholderText('Email *')).toHaveAttribute('name', 'email');
-    expect(getByPlaceholderText('First Name *')).toHaveAttribute('name', 'firstName');
-    expect(getByPlaceholderText('Last Name *')).toHaveAttribute('name', 'lastName');
-    expect(getByPlaceholderText('Password *')).toHaveAttribute('name', 'password');
+    expect(getByPlaceholderText('Email')).toHaveAttribute('name', 'email');
+    expect(getByPlaceholderText('First name')).toHaveAttribute('name', 'firstName');
+    expect(getByPlaceholderText('Last name')).toHaveAttribute('name', 'lastName');
+    expect(getByPlaceholderText('Password')).toHaveAttribute('name', 'password');
   });
 });
