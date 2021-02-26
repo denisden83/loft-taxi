f0a10cb8d85a3ee1faa5b75fb38f1e0fd86fee6f
denys <denisden83@gmail.com>
2021-02-26 01:14:11 +0100

    feat(map): add mapbox

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import Header from "./Header";
+import mapboxgl from "mapbox-gl";
+import withStyles from "@material-ui/core/styles/withStyles";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const Map = (props) => (
-  <>
-    <Header {...props} />
-    <h1>Map</h1>
-  </>
-);
+const styles = () => ({
+  mapWrapper: {
+   position: "relative",
+   width: "100%",
+   height: "100%"
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    height: '50vh'
+  }
+});
 
-export default Map;
+class Map extends React.Component {
+  map = null;
+  mapContainer = React.createRef();
+
+  componentDidMount() {
+    mapboxgl.accessToken = "pk.eyJ1Ijoic2F0YW5zZGVlciIsImEiOiJja2hrcmp6ZWEwOWZxMnNsbjc1NXlrcTd5In0.w964wIfUKgnYjQLp0Ods7Q";
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer.current,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [30.3056504, 59.9429126],
+      zoom: 10,
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    const {classes: {mapWrapper, map}, goToPage} = this.props;
+    return (
+      <>
+        <Header goToPage={goToPage} />
+        <h1>Map</h1>
+        <div className={mapWrapper}>
+          <div className={map} data-testid='map' ref={this.mapContainer}/>
+        </div>
+      </>
+    );
+  }
+}
+
+export default withStyles(styles)(Map);
